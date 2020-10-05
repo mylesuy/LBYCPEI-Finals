4b1538bb1f5eee70d68a932e367fc86156ba3ffc
Mashuu <matthewcharlesllenos@gmail.com>
2020-10-05 18:28:29 +0800

    identification scoring added

--- src/Identification.java
+++ src/Identification.java
@@ -8,17 +8,22 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Identification extends GraphicsProgram {
-    private final int NumPics=10;
+    int NumPics = 0;
+
+    int correct_guesses = 0;
 
     private int num = 1;
+    int index;
 
     int result;
-    String[] answers={"YES","YES","YES","YES","YES"};
+    String[] answer = {"YES", "YES", "YES", "YES", "YES"};
+
     public void run() {
         StartScreen();
         Questions();
         WallPic();
         buttons();
+
     }
 
 
@@ -37,23 +42,24 @@ public class Identification extends GraphicsProgram {
 
 
     }
-    private void Questions(){
-        GLabel question1 = new GLabel("Which one is a scam?");
+
+    private void Questions() {
+        GLabel question1 = new GLabel("Is this a scam?");
         question1.setColor(Color.BLACK);
         question1.setFont("Garamond-20");
-        add(question1,43,42);
+        add(question1, 43, 42);
     }
-    private void WallPic(){
 
-
-
+    private void WallPic() {
 
 
     }
+
     private void nextPhoto() {
         num++;
         drawImages(num);
     }
+
     private void drawImages(int picNum) {
         switch (picNum) {
             case 1 -> {
@@ -62,57 +68,64 @@ public class Identification extends GraphicsProgram {
                 add(pic1, 270, 69);
             }
             case 2 -> {
-                GImage pic2 = new GImage("assets/Identification/2.png");
-                pic2.setSize(200,400);
-                add(pic2,270,69);
+                GImage pic2 = new GImage("assets/Identification/2.jpg");
+                pic2.setSize(200, 400);
+                add(pic2, 270, 69);
             }
             case 3 -> {
-                GImage pic3 = new GImage("assets/Identification/3.png");
-                pic3.setSize(200,400);
-                add(pic3,270,69);
+                GImage pic3 = new GImage("assets/Identification/3.jpg");
+                pic3.setSize(200, 400);
+                add(pic3, 270, 69);
             }
             case 4 -> {
                 GImage pic4 = new GImage("assets/Identification/4.jpg");
-                pic4.setSize(200,400);
-                add(pic4,270,69);
+                pic4.setSize(200, 400);
+                add(pic4, 270, 69);
             }
             case 5 -> {
                 GImage pic5 = new GImage("assets/Identification/5.jpg");
-                pic5.setSize(200,400);
-                add(pic5,270,69);
+                pic5.setSize(200, 400);
+                add(pic5, 270, 69);
             }
             case 6 -> {
                 GImage pic6 = new GImage("assets/Identification/6.jpg");
-                pic6.setSize(200,400);
-                add(pic6,270,69);
+                pic6.setSize(200, 400);
+                add(pic6, 270, 69);
             }
         }
     }
-    private void buttons(){
+
+    private void buttons() {
         drawImages(1);
-        GRect Yes = new GRect(150,100);
+        GRect Yes = new GRect(150, 100);
         Yes.setFillColor(Color.GREEN);
         Yes.setFilled(true);
-        add(Yes,521,241);
-        GRect No = new GRect(150,100);
+        add(Yes, 521, 241);
+        GRect No = new GRect(150, 100);
         No.setFillColor(Color.RED);
         No.setFilled(true);
-        add(No,75,241);
+        add(No, 75, 241);
         GLabel agree = new GLabel("YES");
         agree.setColor(Color.BLACK);
         agree.setFont("Garamond-30");
-        add(agree,555,300);
+        add(agree, 555, 300);
         GLabel No2 = new GLabel("NO");
         No2.setColor(Color.BLACK);
         No2.setFont("Garamond-30");
-        add(No2,125,300);
+        add(No2, 125, 300);
         Yes.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 //remove(pic1);
                 nextPhoto();
-                result++;
 
+                if (e.getSource() == Yes) {
+                    correct_guesses++;
+                    NumPics++;
+                    score();
+
+
+                }
             }
 
             @Override
@@ -141,7 +154,9 @@ public class Identification extends GraphicsProgram {
             public void mouseClicked(MouseEvent e) {
                 //remove(pic1);
                 nextPhoto();
-                result--;
+                NumPics++;
+                score();
+
 
             }
 
@@ -166,8 +181,16 @@ public class Identification extends GraphicsProgram {
             }
         });
     }
-    private void program(){
+
+    public void score() {
+        result = correct_guesses;
+        if (NumPics == 6) {
+            GLabel point = new GLabel(result + "/6");
+            point.setFont("Garamond-30");
+            add(point, 615, 67);
+
+        }
+
 
     }
 }
-
